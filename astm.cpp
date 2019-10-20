41b762ed5b742f00bb8f7e784fa8ba652a42ed6a
IamNotProgrammer <49314448+IamNotProgrammer@users.noreply.github.com>
2019-10-20 20:49:33 +0200

    Add files via upload

--- astm.cpp
+++ astm.cpp
@@ -1,13 +1,8 @@
-#include <iostream>
-#include <iomanip>
-#include <chrono>
+#include "astm.h"
 #include <ctime>
 #include <math.h>
 
-using namespace std::chrono ;
-
-
-double d_d (int y0, int m0, int d0, int h0, int mi0, int s0, int y, int m, int d, int h, int mi, int s) // day difference
+double d_d(int y0, int m0, int d0, int h0, int mi0, int s0, int y, int m, int d, int h, int mi, int s)
 	{
 
 	struct std::tm a = { s0, mi0, h0, d0, m0 - 1, y0 - 1900 } ;
@@ -39,62 +34,3 @@ double GMST(int y, int m, int d, int h, int mi, int s)
 
 	}
 
-int main()
-	{
-
-	int y, y0 ;
-	int m, m0 ;
-	int d, d0 ;
-	int h, h0, mi ;
-	int H, M ;
-	double D, s, S ;
-
-//	std::cout << "Year 0: " ;
-//	std::cin >> y0 ;
-
-//	std::cout << "Month 0: " ;
-//	std::cin >> m0 ;
-
-//	std::cout << "day 0: " ;
-//	std::cin >> d0 ;
-
-//	std::cout << "hour 0: " ;
-//	std::cin >> h0 ;
-
-	std::cout << "Year: " ;	
-	std::cin >> y ;
-
-	std::cout << "Month: " ;
-	std::cin >> m ;
-
-	std::cout << "day: " ;
-	std::cin >> d ;
-
-	std::cout << "hour: " ;
-        std::cin >> h ;
-
-	std::cout << "minute: " ;
-	std::cin >> mi ;
-
-	std::cout << "second: " ;
-	std::cin >> s ;
-
-	high_resolution_clock::time_point t1 = high_resolution_clock::now() ;
-
-	D = GMST(y, m, d, h, mi, s) ;
-	H = int(D) ;
-	M = int( (D - H) * 60) ;
-	S = ( (D - H) * 60 - M ) * 60 ;
-
-	std::cout << std::fixed ;
-	std::cout << std::setprecision(15) ;
-	std::cout << H << ":" << M << ":" << S << "\n" ;
-
-	high_resolution_clock::time_point t2 = high_resolution_clock::now() ;
-
-	auto duration = duration_cast<microseconds>(t2 - t1).count() ;
-
-	std::cout << "\nczas: " << duration << " μs\n" ;  
-
-	return 0 ;
-	}
