a39bacec663e8e1559cbcb5e0f28f209dd1a458c
IamNotProgrammer <49314448+IamNotProgrammer@users.noreply.github.com>
2020-03-16 01:23:44 +0100

    Add files via upload

--- astm.cpp
+++ astm.cpp
@@ -2,6 +2,7 @@
 #include <ctime>
 #include <math.h>
 
+//	  year   month  day    hour   minute  second
 double JD(int y, int m, int d, int h, int mi, float s)
 	{
 
@@ -15,6 +16,8 @@ double JD(int y, int m, int d, int h, int mi, float s)
 
 	}
 
+
+//	   year 0  month 0 day 0   hour 0  minute 0 sec 0     year   month  day    hour   minute  second
 double d_d(int y0, int m0, int d0, int h0, int mi0, float s0, int y, int m, int d, int h, int mi, float s)
 	{
 
@@ -44,11 +47,44 @@ double GMST(int y, int m, int d, int h, int mi, float s)
 		- 0.00000632 * T * T * T ;
 
 	gmst *= 0.0002777777777777778 ;
-	gmst = fmod(gmst, 24.0) ;
-        gmst += ( h + mi * 0.0166666666666 + s * 0.0002777777777777 ) * 1.0027379093382884 ;
+	gmst += ( h + mi * 0.0166666666666 + s * 0.0002777777777777 ) * 1.0027379093382884 ;
 	gmst = fmod(gmst, 24.0) ;
 
 	return gmst ;
 
 	}
 
+
+
+double UTC2TAI(int y, int m, int d, int h, int mi, double s)
+	{
+
+	int n = 2020 - 1972 + 1 ; // limit array
+	double TAI ;
+
+	int leap[n][2]	= { {1,1}, {0,1}, {0,1}, {0,1}, {0,1}, {0,1}, {0,1}, {0,1}, {0,0}, {1,0},
+			    {1,0}, {1,0}, {0,0}, {1,0}, {0,0}, {0,1}, {0,0}, {0,1}, {0,1}, {0,0},
+			    {1,0}, {1,0}, {1,0}, {0,1}, {0,0}, {1,0}, {0,1}, {0,0}, {0,0}, {0,0},
+			    {0,0}, {0,0}, {0,0}, {0,1}, {0,0}, {0,0}, {0,1}, {0,0}, {0,0}, {0,0},
+			    {1,0}, {0,0}, {0,0}, {1,0}, {0,1}, {0,0}, {0,0}, {0,0}, {0,0} } ; // leap seconds each year since 1972.
+										// Yes, I am masochist
+	int l = 10 ; // leap seconds
+	
+	for (int i = 0; i < (y - 1972); i++)
+		{
+
+		if (i == n)
+			break ;
+
+		l += leap[i][0] + leap[i][1] ;
+
+		}
+
+	if ( ( (y - 1972) < n ) and (m > 6) )	
+		l += leap[y - 1972][0] ;
+
+	TAI = h + double(mi) 0.01666666666666666 + (s + double(l)) * 2.777777777777777777e-4 ;
+
+	return TAI ;
+
+	}
