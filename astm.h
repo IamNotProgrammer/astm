1ed79ee80a09c82727141ec3354164616ac54595
IamNotProgrammer <49314448+IamNotProgrammer@users.noreply.github.com>
2020-05-02 03:41:13 +0200

    Add files via upload

--- astm.h
+++ astm.h
@@ -5,7 +5,7 @@ double JD(int, int, int, int, int, double) ;
 
 double d_d(int, int, int, int, int, double, int, int, int, int, int, double) ; 
 			
-double GMST(int, int, int, int, int, float) ;
+double GMST(int, int, int, int, int, double) ;
 					
 double TAI(int, int, int, int, int, double) ;
 
